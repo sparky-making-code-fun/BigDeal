a79af28a060ef942d4c44e09661b76017e2e9d53
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-03-17 17:06:56 -0700

    Added a single test just to make sure I can test

--- venue/tests.py
+++ venue/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Venue
 
-# Create your tests here.
+class TestVenue(TestCase):
+    """Test our venue class"""
+
+    def test_make_venue(self):
+        """Can we even make a venue?"""
+
+        v = Venue.objects.create(venue_name='Howdy')
+        self.assertEqual(v.venue_name, 'Howdy')
