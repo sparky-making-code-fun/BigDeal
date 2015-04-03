3e055b801b9fa6a8ce71d062ad239c7799b4b536
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-04-03 13:16:12 -0700

    Adding stuff for demo lesson

--- venue/urls.py
+++ venue/urls.py
@@ -1,13 +1,11 @@
+__author__ = 'sparky'
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from views import EView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'poker_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^venue/', include('venue.urls'))
+    url(r'eform/$', EView.as_view()),
 )
